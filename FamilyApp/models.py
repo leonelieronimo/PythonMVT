c7d6f01e38b8f4004ca612999e300f5b1dd02f0b
Leonel Ieronimo <leonelieronimo14@gmail.com>
2022-05-19 21:53:56 -0300

    Primer commit: Cargando proyecto

---
+++ FamilyApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+class Familia(models.Model):
+    nombreFamiliar = models.CharField(max_length=20)
+    fechaNacimiento = models.DateField()
+
---
+++ PythonMVT/views.py
@@ -0,0 +1,46 @@
+from asyncore import read
+from datetime import datetime
+from multiprocessing import context
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template import loader
+from FamilyApp.models import Familia
+
+
+def saludo(request):
+    return HttpResponse("Hola coder")
+
+def diaHoy(request):
+    dia = datetime.now()
+    documento_de_texto = f"El dia de hoy es {dia}"
+    return HttpResponse(documento_de_texto)
+
+def nombre(self, nombre):
+
+    nombre = f"Nombre de la persona  es {nombre}"
+    return HttpResponse(nombre)
+
+def saludoTemplate(self):
+    llamado = loader.get_template('template.html')
+    documento = llamado.render()
+    return HttpResponse(documento)
+
+def saludoParametros(self):
+    nom = "Leonel"
+    ap = "Ieronimo"
+    diccionario = {"nombre": nom, "apellido": ap}
+    mi_html = open("C:/Users/USUARIO/Desktop/Programas Electronica/Phyton/EntregaPyhton/PythonMVT/PythonMVT/plantillas/templateParametros.html")
+    plantilla = Template(mi_html.read())
+    mi_html.close()
+    mi_contexto = Context(diccionario)
+    documento = plantilla.render(mi_contexto)
+    return HttpResponse(documento)
+
+def saludoFamilia(self, nombreFamiliar: str = 'Nombre'):
+    
+    plantilla = loader.get_template('templateApp.html')
+    familia = Familia(nombreFamiliar = nombreFamiliar, fechaNacimiento = datetime.now())
+    familia.save()
+    contexto =  {'familia': familia}
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
